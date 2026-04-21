8df777f16bdd9e7abe7aaab0d5e967b3178ccc40
fahrulakbar16 <fahrulakbar16@gmail.com>
2026-04-21 11:20:11 +0800

    feat: redirect root to login and configure logout redirection to login page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,15 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+
+    /**
+     * The user has logged out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    protected function loggedOut(\Illuminate\Http\Request $request)
+    {
+        return redirect()->route('login');
+    }
 }
